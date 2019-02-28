0df1dbb4864ba193626881180af406bf3a51e2e4
akkradet <akkradet7@gmail.com>
2019-02-28 14:39:07 +0700

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -36,5 +36,3 @@ echo "Installing phpMyAdmin..."
 yum -y install phpMyAdmin
 curl -o /etc/httpd/conf.d/phpMyAdmin.conf https://raw.githubusercontent.com/akkradet/lamp/master/phpMyAdmin.conf
 systemctl restart httpd.service
-firewall-cmd --zone=public --add-port=80/tcp --permanent
-firewall-cmd --reload
